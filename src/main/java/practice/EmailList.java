b268dbe8cc1ce7eb8febf3ed15014c073e4e15e4
aleksei_abramov_2 <CQMZ4zCJWWpDYqz>
2022-06-23 21:59:57 +0600

    практическая работа 10.9 выполнена

--- src/main/java/practice/EmailList.java
+++ src/main/java/practice/EmailList.java
@@ -1,19 +1,27 @@
 package practice;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class EmailList {
 
+    private final TreeSet<String> usersEmail = new TreeSet<>();
+
     public void add(String email) {
         // TODO: валидный формат email добавляется, email это строка, она быть может любой
-        // принять решение добавлять аргумент email или нет должен этот метод
+        // принять решение добавлять  email или нет должен этот метод
+        String regex1 = "[abcdefghijklmnopqrstuvwxyz]+@[abcdefghijklmnopqrstuvwxyz]+\\.ru";
+        String regex2 = "[abcdefghijklmnopqrstuvwxyz]+@[abcdefghijklmnopqrstuvwxyz]+\\.com";
+        if (email.matches(regex1) || email.matches(regex2)) {
+            usersEmail.add(email);
+        }else {
+            System.out.println(Main.WRONG_EMAIL_ANSWER);
+        }
     }
 
     public List<String> getSortedEmails() {
         // TODO: возвращается сортированный список электронных адресов в алфавитном порядке
-        return new ArrayList<>();
+        ArrayList<String> list = new ArrayList<>(usersEmail);
+        return list;
     }
 
 }
